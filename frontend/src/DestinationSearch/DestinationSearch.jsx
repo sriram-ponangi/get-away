cf2c93118aa1d5cf36abf074cd59c9135672550e
Jay Gajjar <jy386888@dal.ca>
2021-03-19 16:09:37 -0300

    updated destination search

--- frontend/src/DestinationSearch/DestinationSearch.jsx
+++ frontend/src/DestinationSearch/DestinationSearch.jsx
@@ -32,32 +32,34 @@ class DestinationSearch extends Component {
     render() {
         
         return (
-            <div className='container DestinationSearch'>
-                
-                <form onSubmit={this.searchFormSubmitHandler}>
-                    
-                    <div className="row">
-                        <div className="col-sm-12 hidden-xs">
-                            <h2 className="font-weight-bold text-dark" style={{ fontFamily: 'Verdana' }}>
-                                Search top destinations:
-                            </h2>
-                        </div>
-                        <div className="col-sm-11">
-                            <input className="form-control form-control-lg form-control-borderless"
-                                type="search" placeholder="Country Name" name="countryName" required
-                                onChange={this.countryNameInputChangehandler} />
-                        </div>
-                        <div className="col-sm-1">
-                            <button className="btn btn-lg btn-outline-dark"  type="submit">                            
-                                Search
-                            </button>
+            <div className='container p-5 DestinationSearch'>
+                <div className="row">
+                    <div className="col-md-4 text-center">
+                        <img src={'/img/paper-plane.png'} className="img-responsive paper-plane-dest m-auto d-inline" width="100" alt=""/>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card search-card">
+                            <img className="img-fluid card-img-top" src={world} 
+                            title="Travel The World" alt="Travel The World" />
+                            <div className="card-body">
+                                <h4 className="card-title mb-3">Search Top Destinations:</h4>
+                                <form onSubmit={this.searchFormSubmitHandler}>                    
+                                    <div className="row no-gutters">
+                                        <input className="form-control form-control-lg no-border bg-light"
+                                            type="search" placeholder="Country Name" name="countryName" required
+                                            onChange={this.countryNameInputChangehandler} />
+
+                                        <button className="btn btn-primary btn-lg w-100"  type="submit">                            
+                                            Search
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </form>
-                <hr/>
-                <div>
-                    <img className="img-fluid rounded mx-auto d-block" src={world} 
-                    title="Travel The World" alt="Travel The World" />
+                    <div className="col-md-4 text-center">
+                        <img src={'/img/paper-plane.png'} className="img-responsive paper-plane-dest m-auto d-inline align-bottom" width="100" alt=""/>
+                    </div>
                 </div>
             </div>
         );
