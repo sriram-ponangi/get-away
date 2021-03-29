26778fafac1a5cfd8b8bf030d80892478b82425d
Jay Gajjar <jy386888@dal.ca>
2021-03-29 12:43:11 -0300

    UI updated for Destination Search Results

--- frontend/src/DestinationSearchResult/DestinationSearchResult.jsx
+++ frontend/src/DestinationSearchResult/DestinationSearchResult.jsx
@@ -4,6 +4,7 @@
 */
 import { Component } from 'react';
 import DestinationSearchResultCard from '../DestinationSearchResultCard/DestinationSearchResultCard';
+import './DestinationSearchResult.css';
 import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 
@@ -39,25 +40,27 @@ class DestinationSearchResult extends Component {
         if (this.props.location.countryName !== undefined) {
             if (this.state.topdestinationsList.length > 0) {
                 return (
-                    <div className="container">
-                        <h2 className="font-weight-bold text-dark" style={{ fontFamily: 'Verdana' }}>
-                            Showing results for: {this.props.location.countryName}
-                        </h2>
-                        <hr />
-                        <div className="row">
-                            {
-                                this.state.topdestinationsList.map((result) => (
-                                    <div className="col-md-4">
-                                        <DestinationSearchResultCard
-                                            image={result.imageSource}
-                                            title={result.locationName}
-                                            text={result.description}
-                                            destinationId={result._id}
-                                        // currentUser={this.props.location.currentUser}
-                                        />
-                                    </div>
-                                ))
-                            }
+                    <div className="search-result-wrapper pt-5 pb-5">
+                        <div className="container">
+                            <h2 className="font-weight display-4 text-dark">
+                                Search Results for: <span className="text-pink">"{this.props.location.countryName}"</span>
+                            </h2>
+                            <hr className="mb-4 mt-3" />
+                            <div className="row">
+                                {
+                                    this.state.topdestinationsList.map((result) => (
+                                        <div className="col-md-4 mb-5">
+                                            <DestinationSearchResultCard
+                                                image={result.imageSource}
+                                                title={result.locationName}
+                                                text={result.description}
+                                                destinationId={result._id}
+                                            // currentUser={this.props.location.currentUser}
+                                            />
+                                        </div>
+                                    ))
+                                }
+                            </div>
                         </div>
                     </div>
                 );
--- frontend/src/DestinationSearchResultCard/DestinationSearchResultCard.jsx
+++ frontend/src/DestinationSearchResultCard/DestinationSearchResultCard.jsx
@@ -10,13 +10,13 @@ class DestinationSearchResultCard extends Component {
 
     render() {
         return (
-            <div className="mx-1 DestinationSearchResultCard">
-                <div className="card" style={{ width: '320px' }}>
-                    <img className="card-img-top" src={this.props.image} style={{ width: '100%' }} alt="" />
-                    <div className="card-body">
+            <div className="mx-1 DestinationSearchResultCard h-100">
+                <div className="card h-100 rounded bg-transparent no-border">
+                    <img className="card-img-top mb-2 rounded" width="100%" height="200" src={this.props.image} alt="" />
+                    <div className="card-body bg-dark text-white rounded">
                         <h4 className="card-title">{this.props.title}</h4>
                         <p className="card-text">{this.props.text}</p>                        
-                        <Link className="btn btn-lg btn-outline-dark" to={{                            
+                        <Link className="btn btn-lg btn-outline-light w-100 readmore-btn" to={{                            
                             pathname: '/destination/details/about',
                             destinationId: this.props.destinationId
                                                 
